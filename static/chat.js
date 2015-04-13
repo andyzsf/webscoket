5ca514f7853bc7c5135ede3eee3d39a844e21119
Jonah <thewindbluesoul@126.com>
2015-04-13 22:52:20 +0800

    自写js成功版本

--- static/chat.js
+++ static/chat.js
@@ -1,30 +1,30 @@
 $(document).ready(function () {
-    $("#messageform").click(function () {
-        var message = $(this).formToDict();
+    $("#btn").click(function () {
+        var message = $('#messageform').formToDict();
         window.console.log(message);
         ws.send(JSON.stringify(message));
-    })
-})
-var ws = new WebSocket('ws://localhost:8888/chatsocket');
+        //$('#messageform').find("input[type=text]").val("").select();
+        return false;
+    });
+    //$("#message").select();
+});
+var ws = new WebSocket('ws://' + location.host + '/chatsocket');
 
-ws.onopen = function (msg) {
 
-};
 ws.onmessage = function (msg) {
-    var existing = $("#" + msg.id);
+    var message = JSON.parse(msg.data);
+    var existing = $("#m" + message.id);
     if(existing.length > 0) return;
     var node = $(message.html);
     node.hide();
     $("#inbox").append(node);
     node.slideDown();
 };
-ws.onclose = function (msg) {
 
-};
 
 jQuery.fn.formToDict = function() {
     var fields = this.serializeArray();
-    var json = {}
+    var json = {};
     for (var i = 0; i < fields.length; i++) {
         json[fields[i].name] = fields[i].value;
     }
