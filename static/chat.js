e8d72dbebde59905e5e95b39b3045045906e1dd7
张镇龙 <zhangzhenlong@meiriq.com>
2015-06-01 16:07:17 +0800

    cs229 v1.3

--- static/chat.js
+++ static/chat.js
@@ -1,3 +1,5 @@
+var tag = 1;
+var isusing = false;
 $(document).ready(function () {
     $("#btn").click(function () {
         var message = $('#messageform').formToDict();
@@ -5,6 +7,8 @@ $(document).ready(function () {
         ws.send(JSON.stringify(message));
         //$('#messageform').find("input[type=text]").val("").select();
         $('#message').val("");
+        tag = 0;
+        newMessageRemind.clear();
         return false;
     });
     //$("#message").select();
@@ -14,6 +18,19 @@ var ws = new WebSocket('ws://' + location.host + '/chatsocket');
 
 ws.onmessage = function (msg) {
     var message = JSON.parse(msg.data);
+    if (tag == 1){
+        if (!isusing) {
+            newMessageRemind.show();
+            isusing = true;
+        }
+    }
+    else{
+        // if (isusing) {
+        //    newMessageRemind.clear();
+        //    isusing = false;
+        //}
+        tag = 1;
+    }
     var existing = $("#m" + message.id);
     if(existing.length > 0) return;
     var node = $(message.html);
@@ -31,4 +48,53 @@ jQuery.fn.formToDict = function() {
     }
     if (json.next) delete json.next;
     return json;
+};
+
+
+var newMessageRemind={
+_step: 0,
+_title: document.title,
+_timer: null,
+//显示新消息提示
+show:function(){
+var temps = newMessageRemind._title.replace("【　　　】", "").replace("【新消息】", "");
+newMessageRemind._timer = setTimeout(function() {
+newMessageRemind.show();
+    //这里写Cookie操作
+    newMessageRemind._step++;
+    if (newMessageRemind._step == 3) { newMessageRemind._step = 1 };
+    if (newMessageRemind._step == 1) { document.title = "【　　　】" + temps };
+    if (newMessageRemind._step == 2) { document.title = "【新消息】" + temps };
+    }, 800);
+    return [newMessageRemind._timer, newMessageRemind._title];
+},
+//取消新消息提示
+clear: function(){
+clearTimeout(newMessageRemind._timer );
+document.title = newMessageRemind._title;
+//这里写Cookie操作
+}
+
+};
+
+//document.getElementById("btn").onclick=function(){
+//    if(!isusing){
+//        isusing = true;
+//        newMessageRemind.show();
+//    }
+//};
+
+document.onclick=function(event){
+    event = event || window.event;
+    var isone ="";
+    if(!document.all){
+        isone = event.target.id.toUpperCase();
+    }
+    else{
+        isone = event.srcElement.id.toUpperCase();
+    }
+    if(isone!=="TEST"){
+        isusing = false;
+        newMessageRemind.clear();
+    }
 };
