39fc13c9aeb8ed67d9ae53b44754d68ace0fb7a2
张镇龙 <zhangzhenlong@meiriq.com>
2015-06-06 11:16:12 +0800

    cs229 chatroom v2.1

--- chat.py
+++ chat.py
@@ -34,7 +34,7 @@ class Application(tornado.web.Application):
         settings = {
             "cookie_secret": base64.b64encode(uuid.uuid4().bytes + uuid.uuid4().bytes),
             "template_path": os.path.join(os.path.dirname(__file__), 'templates'),
-            "static_path": os.path.join(os.path.dirname(__file__), '/chat/static'),
+            "static_path": os.path.join(os.path.dirname(__file__), 'static'),
             "xsrf_cookies": "=True",
             "login_url": "/chat/login",
         }
